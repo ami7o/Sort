2f1a933b908c93af80a5e8be2d5445c49959dc82
Ami Oka <amingo.oka@gmail.com>
2020-10-20 14:11:26 -0700

    add selection sort

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Sort
 
 Includes various sorting algorithms.
+
+- Bubble Sort: Swaps two adjacent elements if they are in a wrong order. Worst time O(n^2).
+- Selection Sort: Finds the smallest elements from the unsorted part. Worst time O(n^2).
---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,19 @@
+package sort;
+
+import java.util.List;
+
+public class SelectionSort {
+    public static void sort(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+            int temp = list.get(i);
+            list.set(i, list.get(minIndex));
+            list.set(minIndex, temp);
+        }
+    }
+}
