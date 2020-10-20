c6818364ed21919e73e7cbd08c6989dc322d6a30
Ami Oka <amingo.oka@gmail.com>
2020-10-20 14:23:10 -0700

    add optimization to bubble sort

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Includes various sorting algorithms.
 
-- Bubble Sort: Swaps two adjacent elements if they are in a wrong order. Worst time O(n^2).
-- Selection Sort: Finds the smallest elements from the unsorted part. Worst time O(n^2).
+- Bubble Sort: Swaps two adjacent elements if they are in a wrong order. Worst case O(n^2). It is easy to implement, can detect if the list is sorted to optimize, and very slow for large dataset.
+- Selection Sort: Finds the smallest elements from the unsorted part. Worst case O(n^2).
--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -5,13 +5,16 @@ import java.util.List;
 public class BubbleSort {
     public static void sort(List<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i) > list.get(j)) {
-                    int temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
+            boolean change = false;
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                    change = true;
                 }
             }
+            if (!change) break;
         }
     }
 }
